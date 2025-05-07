14e0ad9de7d5144cc1e0d6f8424a9c79ae87a97b
hiroto-hara <xre0605@gmail.com>
2025-05-07 23:18:15 +0900

    parseテスト

---
+++ arith.ts
@@ -0,0 +1,10 @@
+import { parse } from "npm:tiny-ts-parser";
+
+type Term =
+  | { tag: "true" }
+  | { tag: "false" }
+  | { tag: "if"; cond: Term; thn: Term; els: Term }
+  | { tag: "number"; n: number }
+  | { tag: "add"; left: Term; right: Term };
+
+console.log(parse("1 + 2"));
